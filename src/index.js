48912269d64bfe5e10b4b8f991d4765d31c50afa
Ivan Mitkov <ivan.mitkov@gmail.com>
2019-01-23 16:50:18 +0200

    migrate from react-redux-firebase 2 to 3

--- src/index.js
+++ src/index.js
@@ -3,27 +3,39 @@ import ReactDOM from "react-dom";
 import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import { reduxFirestore, getFirestore } from 'redux-firestore';
-import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import { reduxFirestore, getFirestore,createFirestoreInstance } from "redux-firestore";
+import {
+  ReactReduxFirebaseProvider,
+  getFirebase
+} from "react-redux-firebase";
+
 
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import rootReducer from "./components/store/reducers/rootReducer";
-import fireBaseConfig from './config/fireBaseConfig';
+import fireBaseConfig from "./config/fireBaseConfig";
 
 const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
-    reactReduxFirebase(fireBaseConfig,{}), // redux binding for firebase
     reduxFirestore(fireBaseConfig) // redux bindings for firestore
   )
 );
+const rrfConfig = { userProfile: 'users' } // react-redux-firebase config
+const rrfProps = {
+  firebase:fireBaseConfig,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance // <- needed if using firestore
+}
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
   </Provider>,
   document.getElementById("root")
 );
